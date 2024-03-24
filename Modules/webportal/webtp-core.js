0f317677e3f5f661b380caa7c6363d37936cd4b6
Clickmeharder <69931387+Clickmeharder@users.noreply.github.com>
2024-03-24 03:10:15 -0600

    fin. basic webportal functionalit
    finished basic webportal functionality, plan to fix it to also be able to handle actually reading from csv. for now csv data is directly inside the webtp-core.js

--- Modules/webportal/webtp-core.js
+++ Modules/webportal/webtp-core.js
@@ -1,95 +1,4 @@
-/* function webtpfilterDropdown() {
-    var input, filter, dropdown, options, option, i, txtValue;
-    input = document.getElementById('filterInput');
-    filter = input.value.toUpperCase();
-    dropdown = document.getElementById('webtpoptions');
-    options = dropdown.getElementsByTagName('option');
 
-    for (i = 0; i < options.length; i++) {
-        option = options[i];
-        txtValue = option.textContent || option.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            option.style.display = '';
-        } else {
-            option.style.display = 'none';
-        }
-    }
-} */
-/*------------------------------------------
-regular select filter code - html and js  */
-
-		/*regular select filter html
-		-----------------------------*/
-/*				<select class="webtp" id="eulinkfilter">
-					<option value="option0" selected disabled>Select Filter</option>
-					<option value="option1">Official M.A.</option>
-					<option value="option2">Planetary</option>
-					<option value="option3">Forums</option>
-					<option value="option4">Information & data</option>
-					<option value="option5">Societies</option>
-					<option value="option6">Miscelaneous 3rd Party</option>
-					<option value="option7">Colonist Services & Content</option>
-					<option value="option8">Entropia Creators</option>
-					<option value="option99">All</option>
-				</select>
-*/
-/*--------------------------
-regular select filter js
----------------------------*/
-/*
-document.getElementById('eulinkfilter').addEventListener('change', function() {
-    var filterValue = this.value;
-    var optionsSelect = document.getElementById('webtpoptions');
-
-    // Clear existing options except for the default option
-    while (optionsSelect.options.length > 1) {
-        optionsSelect.remove(1);
-    }
-
-    // Add options based on the selected filter value
-    switch (filterValue) {
-        case 'option1': // Official M.A.
-            optionsSelect.add(new Option('Mindark.com', 'option2'));
-            optionsSelect.add(new Option('Entropiauniverse.com', 'option3'));
-            optionsSelect.add(new Option('PlanetCalypso.com', 'option4'));
-            break;
-        case 'option2': // Planetary
-            // Add options for Planetary filter
-			optionsSelect.add(new Option('PlanetCalypso.com', 'option2'));
-            break;
-        case 'option3': // Forums
-            // Add options for Forums filter
-			optionsSelect.add(new Option('forumexampleurl.com', 'option2'));
-            break;
-        case 'option4': // Information & data
-            // Add options for Information & data filter
-			optionsSelect.add(new Option('entropiawiki.com.com', 'option2'));
-            break;
-        case 'option5': // Societies
-            // Add options for Societies filter
-			optionsSelect.add(new Option('secretuniversalservices.lol', 'option2'));
-            break;
-        case 'option6': // Miscellaneous 3rd Party
-            // Add options for Miscellaneous 3rd Party filter
-			optionsSelect.add(new Option('entropiapartners.com', 'option2'));
-            break;
-        case 'option7': // Colonist Services & Content
-            // Add options for Colonist Services & Content filter
-			optionsSelect.add(new Option('PlanetCalypso.com', 'option2'));
-            break;
-        case 'option8': // Entropia Creators
-            // Add options for Entropia Creators filter
-			optionsSelect.add(new Option('PlanetCalypso.com', 'option2'));
-            break;
-        case 'option99': // All
-            // Add options for All filter
-			optionsSelect.add(new Option('PlanetCalypso.com', 'option2'));
-            break;
-        default:
-            // Add default behavior or error handling
-            break;
-    }
-});*/
 function Togglewebportal(clickedbutton) {
     var portalcontainer = document.getElementById("webtp");
     var portal = document.getElementById("thewebportal");
@@ -129,64 +38,44 @@ function Togglewebportal(clickedbutton) {
         portalimg.style.visibility = "hidden";
     }
 }
-// Function to update the options in the select based on checked checkboxes
+
+// Define CSV data as a string
+const webportalcsvData = `Category,Option,Website Title,Image URL,Website URL
+Official Filter,Mindark.com,Mindark,https://example.com/images/mindark.png,https://www.mindark.com/
+Official Filter,Entropiauniverse.com,Entropia Universe,https://example.com/images/entropiauniverse.png,https://www.entropiauniverse.com/
+Official Filter,PlanetCalypso.com,Planet Calypso,https://example.com/images/planetcalypso.png,https://www.planetcalypso.com/
+Planetary Filter,PlanetCalypso.com,Planet Calypso,https://example.com/images/planetcalypso.png,https://www.planetcalypso.com/
+Forum Filter,forumexampleurl.com,Forum Example,https://example.com/images/forumexample.png,https://www.forumexample.com/
+Information andData,entropiawiki.com.com,Entropia Wiki,https://example.com/images/entropiawiki.png,https://www.entropiawiki.com/
+Societies,secretuniversalservices.lol,Secret Universal Services,https://example.com/images/secretuniversalservices.png,https://www.secretuniversalservices.lol/
+Misc 3rdparty,entropiapartners.com,Entropia Partners,https://example.com/images/entropiapartners.png,https://www.entropiapartners.com/
+Colonist Services,PlanetCalypso.com,Planet Calypso,https://example.com/images/planetcalypso.png,https://www.planetcalypso.com/
+Entropian Creators,PlanetCalypso.com,Planet Calypso,https://example.com/images/planetcalypso.png,https://www.planetcalypso.com/
+My Favourites,Pixelb8.lol,Pixelb8,https://example.com/images/pixelb8.png,https://pixelb8.lol/
+My Portals,mywebsite.lol,My Website,https://example.com/images/mywebsite.png,https://www.mywebsite.com/`;
+// Function to update the options based on checkboxes and CSV data
 function updateWebtpOptions() {
     var optionsSelect = document.getElementById('webtpoptions');
     var checkboxes = document.querySelectorAll('.webtpfilteroptions-container input[type="checkbox"]');
-
+    
     // Clear existing options except for the default option
     while (optionsSelect.options.length > 1) {
         optionsSelect.remove(1);
     }
 
-    // Loop through checkboxes and add options based on checked checkboxes
-    checkboxes.forEach(function(checkbox) {
-        if (checkbox.checked) {
-            switch (checkbox.value) {
-                case 'Official Filter':
-                    optionsSelect.add(new Option('Mindark.com', 'option2'));
-                    optionsSelect.add(new Option('Entropiauniverse.com', 'option3'));
-                    optionsSelect.add(new Option('PlanetCalypso.com', 'option4'));
-                    break;
-                case 'Planetary Filter':
-                    optionsSelect.add(new Option('PlanetCalypso.com', 'option2'));
-                    break;
-                case 'Forum Filter':
-                    optionsSelect.add(new Option('forumexampleurl.com', 'option2'));
-                    break;
-                case 'Information andData':
-                    optionsSelect.add(new Option('entropiawiki.com.com', 'option2'));
-                    break;
-                case 'No Melee Weapons':
-                    optionsSelect.add(new Option('secretuniversalservices.lol', 'option2'));
-                    break;
-                case 'Misc 3rdparty':
-                    optionsSelect.add(new Option('entropiapartners.com', 'option2'));
-                    break;
-                case 'Colonist Services':
-                    optionsSelect.add(new Option('PlanetCalypso.com', 'option2'));
-                    break;
-                case 'Entropian Creators':
-                    optionsSelect.add(new Option('PlanetCalypso.com', 'option2'));
-                    break;
-                case 'My Favourites':
-                    optionsSelect.add(new Option('Pixelb8.lol', 'option2'));
-                    break;
-                case 'My Portals':
-                    optionsSelect.add(new Option('mywebsite.lol', 'option2'));
-                    break;
-                case 'All Options':
-                    optionsSelect.add(new Option('PlanetCalypso.com', 'option2'));
-                    break;
-                default:
-                    // Add default behavior or error handling
-                    break;
-            }
-        }
-    });
+    // Parse CSV data
+    const rows = webportalcsvData.split('\n').slice(1); // Skip header row
+	rows.forEach(row => {
+		const [category, option, title, imageUrl, websiteUrl] = row.split(',');
+		checkboxes.forEach(checkbox => {
+			if (checkbox.checked && checkbox.value === category) {
+				const optionElement = new Option(title, websiteUrl);
+				optionElement.setAttribute('data-image-url', imageUrl); // Store image URL as a data attribute
+				optionsSelect.add(optionElement);
+			}
+		});
+	});
 }
-
-
 
 
 
@@ -287,44 +176,28 @@ function webtpTogglebuttstyle(clickedButton) {
     console.log('webtp filter button clicked:', clickedButton);
 }
 
-document.getElementById('webtpoptions').addEventListener('change', function() {
-    var selectedValue = this.value;
+function Updatewebportal() {
+    var selectedOption = document.getElementById('webtpoptions').options[document.getElementById('webtpoptions').selectedIndex];
     var portalLinktitle = document.getElementById('portalLinktitle');
     var webportalImage = document.getElementById('webportal-image');
 
-    // Update the title text based on the selected option
-    switch (selectedValue) {
-        case 'option0':
-            portalLinktitle.innerText = 'Entropia Partners';
-            break;
-        case 'option1':
-            portalLinktitle.innerText = 'Mindark';
-            break;
-        case 'option2':
-            portalLinktitle.innerText = 'Entropia Universe';
-            break;
-        case 'option3':
-            portalLinktitle.innerText = 'Planet Calypso';
-            break;
-        default:
-            break;
-    }
+    // Get the title, image URL, and website URL from the selected option's data attributes
+    var title = selectedOption.innerText;
+    var imageUrl = selectedOption.getAttribute('data-image-url');
+    var websiteUrl = selectedOption.value; // Assuming the value of the option is the website URL
 
-    // Update the background image of webportal-image based on the selected option
-    switch (selectedValue) {
-        case 'option1':
-            webportalImage.style.backgroundImage = "url('Pixelb8.lol/Modules/webportal/assets/images/entropiapartners-webtp.png')";
-            break;
-        case 'option2':
-            webportalImage.style.backgroundImage = "url('https://www.mindark.com/images/norobot/MetaVerseImage_x770.jpg')";/*"url('Modules/webportal/assets/images/mindark-webtp.png')"*/
-            break;
-        case 'option3':
-            webportalImage.style.backgroundImage = "url('Modules/webportal/assets/images/entropiauniverse-webtp.png')";
-            break;
-        case 'option4':
-            webportalImage.style.backgroundImage = "url('Modules/webportal/assets/images/planetcalypso-webtp.png')";
-            break;
-        default:
-            break;
+    // Update the title text and image based on the selected option
+    portalLinktitle.innerText = title;
+    webportalImage.style.backgroundImage = "url('" + imageUrl + "')";
+
+    // Update the href link of the anchor element inside webportal-image
+    var webportalLink = document.querySelector('#thewebportal a');
+    if (webportalLink) {
+        webportalLink.href = websiteUrl; // Update the href attribute of the anchor element
+    } else {
+        console.error('Anchor element inside webportal-image not found.');
     }
-});
+}
+
+// Event listener for option select
+document.getElementById('webtpoptions').addEventListener('change', Updatewebportal);
