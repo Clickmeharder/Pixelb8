3fae44d248e17cca51c94c66feb485829e884798
Clickmeharder <69931387+Clickmeharder@users.noreply.github.com>
2024-04-06 18:57:42 -0600

    dad

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,8 @@
-// webpack.config.js
 const webpack = require('webpack');
 const dotenv = require('dotenv');
-
-module.exports = () => {
-  // Load environment variables from .env file
-  const env = dotenv.config().parsed;
-
-  // Define environment variables in webpack
-  const envKeys = Object.keys(env).reduce((prev, next) => {
-    prev[`process.env.${next}`] = JSON.stringify(env[next]);
-    return prev;
-  }, {});
-
-  return {
-    // Your webpack configuration settings here
-    plugins: [
-      new webpack.DefinePlugin(envKeys)
-    ]
-  };
+module.exports = {
+  // Other webpack configurations
+  plugins: [
+    new Dotenv(),
+  ],
 };
