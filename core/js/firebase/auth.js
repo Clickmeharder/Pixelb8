09fbae4e6769fe182227e4c01e584afabc8a6447
Clickmeharder <69931387+Clickmeharder@users.noreply.github.com>
2024-04-10 11:42:44 -0600

    Update auth.js

--- core/js/firebase/auth.js
+++ core/js/firebase/auth.js
@@ -49,7 +49,7 @@ function populateAccountDetails(user) {
       { key: 'Status', value: 'Online' },
       { key: 'Username', value: user.displayName || 'No Name Set' },
       { key: 'Profile Username', value: user.displayName || '-idk-' },
-      { key: 'Profile Photo', value: profile.photoURL || 'assets/images/logo/pixelb8logo1.png' },
+      { key: 'Profile Photo', value: user.photoURL || 'null' },
       { key: 'Pixel Count', value: '0px' },
       { key: 'Auth Provider', value: user.providerData[0]?.providerId || 'uknown' },
 	  { key: 'User Email', value: user.email || 'null' }, // This can be populated if needed
