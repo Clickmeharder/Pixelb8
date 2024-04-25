5a039125809c8502f8de45591ad9955f3f36525e
Clickmeharder <69931387+Clickmeharder@users.noreply.github.com>
2024-04-24 22:42:03 -0600

    Update eumaps-core.js

--- Modules/globemaps/js/eumaps-core.js
+++ Modules/globemaps/js/eumaps-core.js
@@ -15,3 +15,33 @@ document.getElementById("maps-scale-down-btn").addEventListener("click", functio
     scaleFactor -= 0.1;
     mapImage.style.transform = `scale(${scaleFactor})`;
 });
+
+	/*     "../../Modules/globemaps/space/spacemap.png",
+    "../../Modules/globemaps/thule/thulemap.png", */
+/*     "../../Modules/globemaps/monria/monriamap.png",
+    "../../Modules/globemaps/dsec_mom/dsec_mommap.png", */
+// Array containing URLs of map images for each planet
+const planetMaps = [
+
+    "../../Modules/globemaps/arkadia/arkadiaglobemap.png",
+	"../../Modules/globemaps/rocktropia/PlanetRocktropiamap.jpg",
+    "../../Modules/globemaps/cyrene/PlanetCyrenemap.jpg",
+    "../../Modules/globemaps/calypso/PlanetCalypsomap.jpg"
+];
+
+// Initialize current map index
+let currentMapIndex = 0;
+
+// Function to cycle through planet maps
+function cyclemapPlanet() {
+    // Increment current map index
+    currentMapIndex++;
+    
+    // Check if current map index exceeds the length of the array
+    if (currentMapIndex >= planetMaps.length) {
+        currentMapIndex = 0; // Reset to the first map if exceeds length
+    }
+    
+    // Update the src attribute of the map image with the new map URL
+    document.getElementById("mapimage").src = planetMaps[currentMapIndex];
+}
