3185fd72e437b2ba955cc63174dbe4efc7a130cf
Clickmeharder <69931387+Clickmeharder@users.noreply.github.com>
2024-06-13 17:00:55 -0600

    Revert "Update SuSOS-core.js"
    This reverts commit 7a1c4042b6cb3b35250bf3cfb8ef7ad4f98ed9d5.

--- Modules/SuSOS/core/js/SuSOS-core.js
+++ Modules/SuSOS/core/js/SuSOS-core.js
@@ -29,8 +29,7 @@ function onPlayerReady(event) {
     total = player.getDuration();
     time = player.getCurrentTime();
 
-    playerTimeDifference = (time / total) * 100;
-    progress(playerTimeDifference, $("#progressBar"));
+    
     $(".current").text(Math.round(time));
   });
 
@@ -55,8 +54,6 @@ function onPlayerReady(event) {
       time = player.getCurrentTime();
       playerTimeDifference = (time / total) * 100;
 
-      // progress(playerTimeDifference, $("#progressBar"));
-
       $(".current").text(Math.round(time));
     }, 1000); // 100 means repeat in 100 ms
   } else {
@@ -66,7 +63,7 @@ function onPlayerReady(event) {
   }
 
   $(".duration").text(Math.floor(total));
-} 
+} */
 
 /*
 Some Commonly used Methods:
@@ -102,13 +99,6 @@ player.setPlaybackQuality(suggestedQuality:String) - Return type Void
 
 */
  
-function progress(percent, $element) {
-  var progressBarWidth = (percent * $element.width()) / 100;
-
-  // $element.find('div').animate({ width: progressBarWidth }, 500).html(percent + "%&nbsp;");
-
-  $element.find("#progressBar").animate({ width: progressBarWidth });
-}
 
 $(document).ready(function (e) {
   $("#mainAudioDial").on("mousemove", function () {
