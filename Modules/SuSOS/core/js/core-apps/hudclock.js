e47905b0b8ffc48ebcc18ce81ad1ed2f04abef78
Clickmeharder <69931387+Clickmeharder@users.noreply.github.com>
2024-06-19 02:28:26 -0600

    Update hudclock.js

--- Modules/SuSOS/core/js/core-apps/hudclock.js
+++ Modules/SuSOS/core/js/core-apps/hudclock.js
@@ -948,17 +948,17 @@ function toggleStopwatchDisplay() {
 
 
 
-var resizeObserver = new ResizeObserver(function(entries) {
+/* var resizeObserver = new ResizeObserver(function(entries) {
 	for (let entry of entries) {
 		if (entry.target === calendarElement) {
 			calendar.render();
 		}
 	}
-});
+}); */
+			// Optionally, if you want to refresh on window resize
+/* 	window.addEventListener('resize', calendar.render); */
 // Add event listener to the clocktools-button
 document.getElementById('clocktools-button').addEventListener('click', toggleStopwatchDisplay);
 
 // Add click event listener
 	calendarElement.addEventListener('click', calendar.render);
-			// Optionally, if you want to refresh on window resize
-	window.addEventListener('resize', calendar.render);
