17e3bf8f7036dcd98dafd23dde1b1fcbb347595a
Clickmeharder <69931387+Clickmeharder@users.noreply.github.com>
2024-06-19 19:26:42 -0600

    Update EuItemStash-Applet.js

--- Modules/SuSOS/apps/winblows69/EU-Applets/EuItemStash/js/EuItemStash-Applet.js
+++ Modules/SuSOS/apps/winblows69/EU-Applets/EuItemStash/js/EuItemStash-Applet.js
@@ -264,7 +264,8 @@ function copyTablesFancyHtml(tableIds, colonistName) {
 	htmlContent.push('    font-weight:;');
 	htmlContent.push('    border:0px solid #524e4e;');
 	htmlContent.push('    color: #524e4e;">');
-	htmlContent.push('    Colonist:' + ${colonistName} + '</h1>');
+	htmlContent.push('    Colonist:');
+	htmlContent.push(${colonistName});
 	htmlContent.push('</h1>');
 	htmlContent.push('</div>');
     // Add the button group on the right side
