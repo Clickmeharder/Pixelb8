ba2a741b5a4ba3807b7c196220fddf985dbe9165
Clickmeharder <69931387+Clickmeharder@users.noreply.github.com>
2024-06-30 04:13:51 -0600

    Update EuItemStash-Applet.js

--- Modules/SuSOS/apps/winblows69/EU-Applets/EuItemStash/js/EuItemStash-Applet.js
+++ Modules/SuSOS/apps/winblows69/EU-Applets/EuItemStash/js/EuItemStash-Applet.js
@@ -232,7 +232,10 @@ function copyTablesFancyHtml(tableIds, colonistName) {
 	htmlContent.push('<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.js"></script>');
 	htmlContent.push('</head>');
 	// create a body element to wrap the page div that contains all of the content.
-	htmlContent.push('<body>');
+	htmlContent.push('<body ');
+	htmlContent.push('style="background-image:');
+	htmlContent.push(`${userImg}`);
+	htmlContent.push(';">');
     // Create a wrapper div for all of the page content inside the body
     htmlContent.push('<div style="background-color: #2b2a2a; padding: 10px;max-height:100%; max-width: 99vw; margin: 0 auto; position: relative;opacity:0.8;">');
 
@@ -351,7 +354,7 @@ function copyTablesFancyHtml(tableIds, colonistName) {
 					}
 					body {
 						background-color: #333;
-						background-image: url(`${userImg}`);
+						background-image:;
 						background-size: cover;
 						background-repeat: no-repeat;
 						background-position: center;
