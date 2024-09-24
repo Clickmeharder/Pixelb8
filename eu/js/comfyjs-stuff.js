99c2b458fd49f37cfb586db0ee7a1b0177ecb429
Clickmeharder <69931387+Clickmeharder@users.noreply.github.com>
2024-09-23 19:18:27 -0600

    comfyjs-stuff

---
+++ eu/js/comfyjs-stuff.js
@@ -0,0 +1,6 @@
+  ComfyJS.onCommand = ( user, command, message, flags, extra ) => {
+	if( flags.broadcaster && command === "test" ) {
+	  console.log( "!test was typed in chat" );
+	}
+  }
+  ComfyJS.Init( "jaedraze" );
