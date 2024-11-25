83cc4b66b1e05915917d4160a00d0d893a749d0b
Clickmeharder <69931387+Clickmeharder@users.noreply.github.com>
2024-11-24 18:25:28 -0700

    Update auth.js

--- core/js/firebase/auth.js
+++ core/js/firebase/auth.js
@@ -128,13 +128,14 @@ getDocs(collection(db, 'UserProfiles'))
           console.log("profile Email: " + profile.email);
 		  console.log("user Email: " + user.email);
           console.log("Photo URL: " + profile.photoURL);
-		  console.log("users pixels: " + user.pixelcount);
+		  console.log("users pixels: " + profile.pixelcount);
           statusElement.textContent = "Status: Online";
           userauthproveriderElement.textContent = profile.providerId;
 		  usernameElement.textContent = user.displayName || "Nameless";
           profileusernameElement.textContent = profile.displayName || "-idk-";
           emailElement.textContent = profile.email || "Unknown";
           photoElement.src = user.photoURL || "default.jpg";
+		  userpixelcountElement.textContent = profile.pixelcount || "null";
 		  userpixelcountElement.classList.remove('hidden');
           emailVerifiedElement.textContent = user.emailVerified ? "► Verified" : " ► Unverified";
           emailVerifiedElement.classList.remove('hidden');
