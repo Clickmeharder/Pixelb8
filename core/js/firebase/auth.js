3a19cb373a8aa66cbf3adf27f3d258626a00e7ef
Clickmeharder <69931387+Clickmeharder@users.noreply.github.com>
2024-11-28 18:54:44 -0700

    Update auth.js

--- core/js/firebase/auth.js
+++ core/js/firebase/auth.js
@@ -126,7 +126,6 @@ getDocs(collection(db, 'UserProfiles'))
 				// Log the user's pixelBalance field
 			      const userData = doc.data();
 				    console.log("Logged-in user's pixelBalance:", userData.balancePixels);
-					console.log(doc.id, userData.balancePixels);
 					
 					userpixelcountElement.textContent = "Pixels: " + userData.balancePixels || "null";//fix this to properly set it to the logged in users balancePixels
 			    }
