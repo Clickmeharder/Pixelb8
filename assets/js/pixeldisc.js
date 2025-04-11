2731c43e73d2de26f7bc22005567bac6ec6f88ec
Clickmeharder <69931387+Clickmeharder@users.noreply.github.com>
2025-04-11 07:03:33 -0600

    Update pixeldisc.js

--- assets/js/pixeldisc.js
+++ assets/js/pixeldisc.js
@@ -26,7 +26,7 @@ function debugThemeStyles() {
 	colors = [color1, color2];
 	wheels = null;
 	frame = null;
-
+	repaint(angle)
 }
 // Fallback if the CSS variable isn't set
 const wheelFontConfig = {
