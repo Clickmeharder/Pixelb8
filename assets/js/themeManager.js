2566c7ea8a32f48d545a810b631633a816d5d474
Clickmeharder <69931387+Clickmeharder@users.noreply.github.com>
2025-04-13 00:42:08 -0600

    eep

--- assets/js/themeManager.js
+++ assets/js/themeManager.js
@@ -11,6 +11,8 @@ const sliders = [
   { id: "wheelX", unit: "%", variable: "--wheel-X", min: 0, max: 90 },
   { id: "wheelResultY", unit: "%", variable: "--wheelResult-Y", min: 0, max: 100 },
   { id: "wheelResultX", unit: "%", variable: "--wheelResult-X", min: 0, max: 100 },
+  { id: "wheelLeverY", unit: "%", variable: "--wheelLever-Y", min: 0, max: 100 },
+  { id: "wheelLeverX", unit: "%", variable: "--wheelLever-X", min: 0, max: 100 },
 ];
 const wheelSliders = [
   { id: "wheelY", unit: "%", variable: "--wheel-Y", min: 0, max: 66 },
@@ -100,6 +102,8 @@ function setTheme(themeName, saveLayout = true) {
     "--wheel-X": getComputedStyle(document.documentElement).getPropertyValue("--twitchchat-X"),
 	"--wheelResult-Y": getComputedStyle(document.documentElement).getPropertyValue("--wheelResult-Y"),
     "--wheelResult-X": getComputedStyle(document.documentElement).getPropertyValue("--wheelResult-X"),
+    "--wheelLever-Y": getComputedStyle(document.documentElement).getPropertyValue("--wheelLever-Y"),
+    "--wheelLever-X": getComputedStyle(document.documentElement).getPropertyValue("--wheelLever-X"),
   } : {};
 
   // Save both theme and layout
