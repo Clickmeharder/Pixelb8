9f353d46b05942f417575bfc1acc79fd84771c43
Jae Draze <69931387+Clickmeharder@users.noreply.github.com>
2026-01-17 07:56:24 -0800

    Update test.js

--- assets/js/test.js
+++ assets/js/test.js
@@ -812,12 +812,12 @@ function drawCapeItem(ctx, p, anchors, item) {
     ctx.beginPath();
     
     // Start at neck (Higher up than before)
-    ctx.moveTo(headX, p.y - 30 + anchors.bodyY); 
+    ctx.moveTo(headX, p.y - 40 + anchors.bodyY); 
     
     // Left side of cape
     ctx.quadraticCurveTo(headX - 25, p.y + 10 + anchors.bodyY, centerX - 18, p.y + 42 + anchors.bodyY);
     // Bottom edge
-    ctx.lineTo(centerX + 18, p.y + 42 + anchors.bodyY);
+    ctx.lineTo(centerX + 18, p.y + 30 + anchors.bodyY);
     // Right side of cape back to neck
     ctx.quadraticCurveTo(headX + 25, p.y + 10 + anchors.bodyY, headX, p.y - 20 + anchors.bodyY);
     
