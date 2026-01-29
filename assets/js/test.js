2e42f7f810bf33379aad790c4f2dc40fd56b15ca
Jae Draze <69931387+Clickmeharder@users.noreply.github.com>
2026-01-29 13:38:09 -0800

    Update test.js

--- assets/js/test.js
+++ assets/js/test.js
@@ -5989,7 +5989,7 @@ document.addEventListener("DOMContentLoaded", () => {
 //==========================
 //RUNNING ON LOAD
 gameLoop();
-//localStorage.removeItem("rpg_jaedraze");
-localStorage.removeItem("rpg_JaeDraze");
+
+//localStorage.removeItem("rpg_JaeDraze");
 //localStorage.removeItem("rpg_Player1");
 //localStorage.removeItem("rpg_player1");
