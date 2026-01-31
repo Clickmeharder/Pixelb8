0c58d75a0abfdab64768cf14e9d2de12f6034126
Jae Draze <69931387+Clickmeharder@users.noreply.github.com>
2026-01-30 21:26:01 -0800

    Update itemlibrary.js

--- core/js/shared/stickmenfall/itemlibrary.js
+++ core/js/shared/stickmenfall/itemlibrary.js
@@ -1987,11 +1987,6 @@ const POSE_LIBRARY = {
 		return {
 			left: handL,
 			right: handR,
-			
-			// Joint flare
-			leftKnee: kneeL,
-			rightKnee: kneeR,
-			
 			// Ground contact
 			leftFoot: footL,
 			rightFoot: footR
